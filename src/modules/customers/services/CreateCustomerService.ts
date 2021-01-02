80bf4a38240d713a4deb52b8e80418c5c4840e94
Ricky Almeida <ricky.almeida@nave.rs>
2021-01-02 18:05:47 -0300

    feat: create customer

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,24 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const foundCustomer = await this.customersRepository.findByEmail(email);
+
+    if (foundCustomer) {
+      throw new AppError('This email is already in use');
+    }
+
+    const customer = await this.customersRepository.create({
+      name,
+      email,
+    });
+
+    return customer;
   }
 }
 
